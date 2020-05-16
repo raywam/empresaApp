c0dec124318d3b358d70400e12dea1bd9a41eb56
Raywam <raywam@hotmail.com>
2020-05-15 22:35:51 -0300

    Projeto EmpresaApp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { MainComponent } from './pages/main/main.component';
+import { MenuComponent } from './shared/menu/menu.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { PageTitleComponent } from './shared/page-title/page-title.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    MenuComponent,
+    DashboardComponent,
+    PageTitleComponent,
   ],
   imports: [
     BrowserModule,
